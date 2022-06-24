736e28fcb841945e610bd16cb59e210f66c69f48
Diksha Rani <83601273+Dik17@users.noreply.github.com>
2022-06-24 18:14:47 +0530

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,60 @@
-import React from 'react';
-import '../../App.css';
+import React,{useState} from 'react';
+import './login.css';
+
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+	const [details,setDetails]=useState({name:"",email:"",password:"",confirmpass:""});
+	const submitHandler=e=>{
+	  e.preventDefault();
+	console.log(details)
+	}
+  return(
+    <div>
+<div class="container">
+	<div class="screen">
+		<div class="screen__content">
+			<form class="login" onSubmit={submitHandler}>
+      <div class="login__field">
+					<i class="login__icon fas fa-user"></i>
+					<input type="text" class="login__input" placeholder="User name " name="name" onChange={e=>setDetails({...details,name:e.target.value}) }  value={details.name}></input>
+				</div>
+				<div class="login__field">
+					<i class="login__icon fas fa-user"></i>
+					<input type="text" class="login__input" placeholder=" Email" name='email' onChange={e=>setDetails({...details,email:e.target.value}) }  value={details.email} ></input>
+				</div>
+				<div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+					<input type="password" class="login__input" placeholder="Password" name='password' onChange={e=>setDetails({...details,password:e.target.value}) }  value={details.password}></input>
+				</div>
+        <div class="login__field">
+
+					<i class="login__icon fas fa-lock"></i>
+					<input type="password" class="login__input" placeholder="Confirm Password" name='confirmpass' onChange={e=>setDetails({...details,confirmpass:e.target.value}) }  value={details.confirmpass}></input>
+				</div>
+     
+				<button class="button login__submit" >
+					<span class="button__text">Sign Up Now</span>
+					<i class="button__icon fas fa-chevron-right"></i>
+				</button>				
+			</form>
+			{/* <div class="social-login">
+				<h3>log in via</h3>
+				<div class="social-icons">
+					<a href="#" class="social-login__icon fab fa-instagram"></a>
+					<a href="#" class="social-login__icon fab fa-facebook"></a>
+					<a href="#" class="social-login__icon fab fa-twitter"></a>
+				</div>
+			</div> */}
+		</div>
+		<div class="screen__background">
+			<span class="screen__background__shape screen__background__shape4"></span>
+			<span class="screen__background__shape screen__background__shape3"></span>		
+			<span class="screen__background__shape screen__background__shape2"></span>
+			<span class="screen__background__shape screen__background__shape1"></span>
+		</div>		
+	</div>
+</div>
+</div>
+
+  )
 }
